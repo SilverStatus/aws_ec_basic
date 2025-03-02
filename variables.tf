bfdaa39f6db9da105549120df3a7ce37119b97f7
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-03-02 15:52:56 +0700

    add contents to tf files

--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "aws" {
     default = "us-east-1"
     type = string
 }
+
+variable "main_vpc_cidr" {
+    default = "10.0.0.0/24"
+}
+
+variable "public_subnets" {
+    default = "10.0.0.128/26"
+}
+
+variable "private_subnets" {
+    default = "10.0.0.192/26"
+}
